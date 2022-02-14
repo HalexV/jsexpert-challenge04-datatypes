a589dca7c7fb31729632b13b36ba04bb013fadeb
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-14 16:55:40 -0400

    feat: should convert the user object to the string primitive with object id when string conversion is applied

--- src/entity/User.js
+++ src/entity/User.js
@@ -2,7 +2,10 @@ class User {
   constructor({ id }) {
     this.id = id;
   }
-  // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  
+  [Symbol.toPrimitive]() {
+    return `${this.id}`
+  }
 }
 
 export default User;
