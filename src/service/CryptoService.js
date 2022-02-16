dddf4ad4ff7566f7f8d55ec029445011e31f2093
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-16 10:47:37 -0400

    feat: should finish when the repository throws

--- src/service/CryptoService.js
+++ src/service/CryptoService.js
@@ -7,6 +7,21 @@ class CryptoService {
   }
   async *list() {
     // TODO: implementar generator que chama a repository fazendo a paginação
+    let data = null
+    let page = 1
+    let response = null
+
+    while(1){
+      try {
+        response = await this.repository.list(++page)
+        data = response.data
+
+        yield new Crypto(data)
+      } catch (error) {
+        console.error(error)
+        break
+      }
+    }
   }
 }
 
