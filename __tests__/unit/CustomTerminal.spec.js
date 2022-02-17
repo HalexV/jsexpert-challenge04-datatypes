9f1404accac5b13acc048097a5db17194da68e5c
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-17 10:03:02 -0400

    test: addDataToPrint should be able to add data to the structure data

--- __tests__/unit/CustomTerminal.spec.js
+++ __tests__/unit/CustomTerminal.spec.js
@@ -10,6 +10,37 @@ describe('CustomTerminal', () => {
       expect(customTerminal.dataSize()).toBe(0)
     })
   })
+
+  describe('addDataToPrint', () => {
+    it('should be able to add data to the structure data', () => {
+      const customTerminal = new CustomTerminal()
+
+      const fakeData = [
+        {
+          id: '1',
+          any1: 'any1',
+          any2: 'any2',
+          any3: 'any3'
+        },
+        {
+          id: '2',
+          any1: 'any1',
+          any2: 'any2',
+          any3: 'any3'
+        },
+        {
+          id: '3',
+          any1: 'any1',
+          any2: 'any2',
+          any3: 'any3'
+        }
+      ]
+
+      customTerminal.addDataToPrint(fakeData)
+
+      expect(customTerminal.dataSize()).toBe(3)
+    })
+  })
   
   
 })
